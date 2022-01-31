abe4cc41e61dd37498bf4fb63f64ce2a29c51392
yu00hun <yu00hun@naver.com>
2022-01-31 16:51:59 +0900

    complete

---
+++ src/Baekjoon_10926.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Baekjoon_10926 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		String str = scan.nextLine();
+		
+		System.out.println(str+"??!");
+		
+	}
+
+}
