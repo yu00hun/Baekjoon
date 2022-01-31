272845e24cee437fdddcb9b4dc7dba868f57ea30
yu00hun <yu00hun@naver.com>
2022-01-31 16:57:08 +0900

    complete

---
+++ src/Baekjoon_18108.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Baekjoon_18108 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int year = scan.nextInt();
+		
+		System.out.println(year -543);
+
+	}
+
+}
