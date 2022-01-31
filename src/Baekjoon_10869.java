57d6192745b308c41e49ac997680ee7e54928f90
yu00hun <yu00hun@naver.com>
2022-01-31 16:48:48 +0900

    complete

---
+++ src/Baekjoon_10869.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Baekjoon_10869 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+
+	}
+
+}
