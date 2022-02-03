d669eb896c45679b13b77246d6b7254169e03f75
yu00hun <yu00hun@naver.com>
2022-02-03 15:46:28 +0900

    complete

---
+++ src/Baekjoon_2741.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Baekjoon_2741 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		
+		for(int i=1; i<=n; i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}
