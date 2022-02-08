b85853d21a862fb0be600b1533cae25431e63646
yu00hun <yu00hun@naver.com>
2022-02-08 22:04:44 +0900

    new solution

--- src/Baekjoon_3052.java
+++ src/Baekjoon_3052.java
@@ -22,3 +22,34 @@ public class Baekjoon_3052 {
 	}
 
 }
+
+/*
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Baekjoon_3052 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int result[] = new int[10];
+		int cnt=1;
+		
+		for(int i=0; i<10; i++) {
+			result[i] = scan.nextInt()%42;
+		}
+		
+		Arrays.sort(result);
+		
+		for(int j=0; j<9; j++) {
+			if(result[j] != result[j+1]) {
+				cnt++;
+			}
+		}
+		
+		System.out.println(cnt);
+	}
+
+}
+*/
